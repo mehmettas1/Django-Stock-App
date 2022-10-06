35875612d7e0f21774287d177e387388a4550c66
mehmet <tas349873@gmail.com>
2022-10-06 17:10:21 +0300

    update

--- stock/api/views.py
+++ stock/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 from rest_framework import permissions
+from stock.api.permissions import ProductManagerPermission
 from stock.models import Category, Brand, Firm, Product, Transaction
 from .serializers import CategorySerializer, FirmSerializer, BrandSerializer, TransactionSerializer, ProductSerializer
 from django.db.models import Sum
@@ -8,26 +9,31 @@ from django.db.models import Sum
 class CategoryView(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    permission_classes = [permissions.DjangoModelPermissions]
 
 
 class BrandView(viewsets.ModelViewSet):
     queryset = Brand.objects.all()
     serializer_class = BrandSerializer
+    permission_classes = [permissions.DjangoModelPermissions]
 
 
 class FirmView(viewsets.ModelViewSet):
     queryset = Firm.objects.all()
     serializer_class = FirmSerializer
+    permission_classes = [permissions.DjangoModelPermissions]
 
 
 class ProductView(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = [permissions.DjangoModelPermissions]
 
 
 class TransactionView(viewsets.ModelViewSet):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
+    # permission_classes = [ProductManagerPermission]
 
     
 
